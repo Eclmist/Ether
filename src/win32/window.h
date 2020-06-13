11656988daf167417f87fc9d95c224908121dc99
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-06-14 00:26:34 +0800

    Refactor WinAPI code
    * Fixed platform detection in CMakeList
    * Temporarily commented DX12 code (refactor in-progress)
    * Added application icon

---
+++ src/win32/window.h
@@ -0,0 +1,55 @@
+/*
+    This file is part of Ether, an open-source DirectX 12 renderer.
+
+    Copyright (c) 2020-2021 Samuel Van Allen - All rights reserved.
+
+    Ether is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "ethwin.h"
+
+class Window
+{
+public:
+    Window(int width, int height, const wchar_t* windowTitle);
+    ~Window();
+
+public:
+    Window(const Window&) = delete;
+    Window& operator=(const Window&) = delete;
+
+public:
+    void SetFullscreen(bool isFullscreen);
+
+private:
+    void InitWindow(const wchar_t* windowTitle);
+    void CentralizeClientRect(int screenWidth, int screenHeight, int clientWidth, int clientHeight);
+    RECT GetCurrentMonitorRect() const;
+    void RegisterWindowClass() const noexcept;
+
+private:
+    static LRESULT CALLBACK WndProcSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    LRESULT WndProcInternal(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+private:
+    // Handle toggling of fullscreen
+    bool m_IsFullscreen;
+    RECT m_WindowedRect;
+
+    // Handle to the actual win32 window and window instance
+    HWND m_hWnd;
+    HINSTANCE m_hInst;
+};
