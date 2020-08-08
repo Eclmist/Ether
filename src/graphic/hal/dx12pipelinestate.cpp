d64b877fc1914e4f08f65365eb2ea6d564928775
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-08-09 01:45:07 +0800

    Add PSO hal class

---
+++ src/graphic/hal/dx12pipelinestate.cpp
@@ -0,0 +1,32 @@
+/*
+    This file is part of Ether, an open-source DirectX 12 renderer.
+
+    Copyright (c) 2020-2021 Samuel Van Allen - All rights reserved.
+
+    Ether is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "dx12pipelinestate.h"
+
+DX12PipelineState::DX12PipelineState(wrl::ComPtr<ID3D12Device3> device, DX12PipelineStateStream& pipelineStateStream)
+{
+    // Our PipelineStateStream needs to be wrapped in the proper dx12 desc structure
+    D3D12_PIPELINE_STATE_STREAM_DESC pipelineStateStreamDesc = {
+        sizeof(DX12PipelineStateStream), &pipelineStateStream
+    };
+
+    ThrowIfFailed(device->CreatePipelineState(&pipelineStateStream, IID_PPV_ARGS(&m_PipelineState)));
+}
