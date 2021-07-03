19b3909a1fe3482cd52825de7c101ddc57085338
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2021-07-03 21:58:02 +0800

    Add command line options

---
+++ src/system/win32/commandlineoptions.cpp
@@ -0,0 +1,51 @@
+/*
+    This file is part of Ether, an open-source DirectX 12 renderer.
+
+    Copyright (c) 2020-2021 Samuel Van Allen - All rights reserved.
+
+    Ether is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "commandlineoptions.h"
+#include <shellapi.h>
+#include <iostream>
+
+ETH_NAMESPACE_BEGIN
+
+namespace Win32
+{
+
+CommandLineOptions::CommandLineOptions()
+    : m_DebugMode(false)
+{
+}
+
+void CommandLineOptions::Initialize()
+{
+    int argc;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLine(), &argc);
+
+    for (int i = 0; i < argc; ++i)
+        InitializeArg(argv[i]);
+}
+
+void CommandLineOptions::InitializeArg(const std::wstring& arg)
+{
+    if (arg == L"-debug")
+        m_DebugMode = true;
+}
+
+}
+
+ETH_NAMESPACE_END
