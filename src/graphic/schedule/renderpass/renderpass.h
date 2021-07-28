be14eeca750fdaac8de9a5826d9218c517ad2ddd
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2021-07-28 17:17:44 +0800

    Add render pass abstractions

---
+++ src/graphic/schedule/renderpass/renderpass.h
@@ -0,0 +1,36 @@
+/*
+    This file is part of Ether, an open-source DirectX 12 renderer.
+
+    Copyright (c) 2020-2021 Samuel Van Allen - All rights reserved.
+
+    Ether is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+ETH_NAMESPACE_BEGIN
+
+class RenderPass : public NonCopyable
+{
+public:
+    RenderPass(const std::string& name);
+
+    virtual void RegisterInputOutput() = 0;
+    virtual void Render(GraphicContext& context) = 0;
+
+private:
+    std::string m_Name;
+};
+
+ETH_NAMESPACE_END
