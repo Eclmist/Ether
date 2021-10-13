f13cfeb86d022f66e7221d53350eeb32fc50666f
Samuel Huang <samuel.huanghaoyi@gmail.com>
2021-10-13 19:12:06 +0800

    Create GUID for Serializable classes

---
+++ src/system/serializable.h
@@ -0,0 +1,58 @@
+/*
+    This file is part of Ether, an open-source DirectX 12 renderer.
+
+    Copyright (c) 2020-2021 Samuel Van Allen - All rights reserved.
+
+    Ether is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <Combaseapi.h>
+
+ETH_NAMESPACE_BEGIN
+
+/*
+    Derive from this class to make your classes serializable
+    
+    TODO: Actually support serialization/deserialization. Right now this class
+    only stores the GUID to be serialized through toolmode IPC
+*/
+
+class Serializable
+{
+public:
+    Serializable()
+    {
+        CoCreateGuid(&m_GUID);
+    };
+
+    ~Serializable() = default;
+
+    inline GUID GetGuid() const { return m_GUID; }
+
+    inline std::string GetGuidString() const {
+        char output[40];
+        snprintf(output, 40, "%08X-%04hX-%04hX-%02X%02X-%02X%02X%02X%02X%02X%02X",
+            m_GUID.Data1, m_GUID.Data2, m_GUID.Data3,
+            m_GUID.Data4[0], m_GUID.Data4[1], m_GUID.Data4[2], m_GUID.Data4[3],
+            m_GUID.Data4[4], m_GUID.Data4[5], m_GUID.Data4[6], m_GUID.Data4[7]);
+        return std::string(output);
+    }
+
+protected:
+    GUID m_GUID;
+};
+
+ETH_NAMESPACE_END
