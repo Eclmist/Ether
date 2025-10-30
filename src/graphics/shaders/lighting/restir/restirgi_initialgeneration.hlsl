de393290507e3ecaa45514ae10b7dca57d631ec6
Eclmist <eclmist@gmail.com>
2025-10-31 02:58:33 +0900

    [Pathtracer] Refactor part7

--- src/graphics/shaders/lighting/restir/restirgi_initialgeneration.hlsl
+++ src/graphics/shaders/lighting/restir/restirgi_initialgeneration.hlsl
@@ -19,50 +19,6 @@
 
 #include "lighting/restir/gireservoirresampling.hlsl"
 
-#define USE_IMPORTANCE_SAMPLING 1
-
-void SampleDirectionBrdf(ShadingSurface surface, out float3 wi, out float pdf)
-{
-    const uint2 sampleCoords = DispatchRaysIndex().xy;
-    const uint2 bufferSize = DispatchRaysDimensions().xy;
-    const uint sampleIdx = GetSampleIndexFromSampleCoords(sampleCoords, bufferSize);
-    const float2 rand2D = CMJ_Sample2D(sampleIdx, 1024, 1024, g_GlobalConstants.m_FrameNumber);
-
-    const float3 wo = normalize(g_GlobalConstants.m_CameraPosition.xyz - surface.m_Position);
-    const float diffuseWeight = lerp(lerp(0.5, 1.0, surface.m_Roughness), 0.0, surface.m_Metalness);
-    const float specularWeight = 1.0 - diffuseWeight;
-
-    const bool importanceSampleBrdf = Random(rand2D.x) <= specularWeight;
-
-    if (importanceSampleBrdf)
-        wi = normalize(ImportanceSampleGGX(rand2D, wo, surface.m_Normal, surface.m_Roughness));
-    else
-        wi = normalize(TangentToWorld(SampleDirectionCosineHemisphere(rand2D), surface.m_Normal));
-
-    const float3 H = normalize(wi + wo);
-    const float nDotH = saturate(dot(surface.m_Normal, H));
-    const float nDotV = saturate(dot(surface.m_Normal, wo));
-    const float vDotH = saturate(dot(wo, H));
-
-    // TODO: Abs is also wrong here. Why does it work?
-    const float cosTheta = abs(dot(-wi, surface.m_Normal));
-
-    if (importanceSampleBrdf)
-        pdf = UE4JointPdf(specularWeight, nDotH, cosTheta, vDotH, surface.m_Roughness);
-    else
-        pdf = SampleDirectionHemisphere_Pdf();
-}
-
-void SampleDirectionUniform(ShadingSurface surface, out float3 wi, out float pdf)
-{
-    const uint2 sampleCoords = DispatchRaysIndex().xy;
-    const uint2 bufferSize = DispatchRaysDimensions().xy;
-    const uint sampleIdx = GetSampleIndexFromSampleCoords(sampleCoords, bufferSize);
-    const float2 rand2D = CMJ_Sample2D(sampleIdx, 1024, 1024, g_GlobalConstants.m_FrameNumber);
-    wi = TangentToWorld(SampleDirectionHemisphere(rand2D), surface.m_Normal);
-    pdf = SampleDirectionHemisphere_Pdf();
-}
-
 [shader("raygeneration")]
 void RayGeneration()
 {
@@ -72,18 +28,19 @@ void RayGeneration()
     const uint sampleIdx = GetSampleIndexFromSampleCoords(sampleCoords, bufferSize);
 
     const ShadingSurface surface = GetShadingSurfaceFromGBuffers(screenCoords, g_GBufferA, g_GBufferB, g_GBufferC, g_GBufferD);
+    const float3 viewDir = normalize(g_GlobalConstants.m_CameraPosition.xyz - surface.m_Position);
     const float3 wo = normalize(g_GlobalConstants.m_CameraPosition.xyz - surface.m_Position);
 
     float3 wi;
     float pdf;
 
 #if USE_IMPORTANCE_SAMPLING
-    SampleDirectionBrdf(surface, wi, pdf);
+    SampleDirectionBrdf(surface, g_GlobalConstants.m_FrameNumber, viewDir, wi, pdf);
 #else
-    SampleDirectionUniform(surface, wi, pdf);
+    SampleDirectionUniform(surface, g_GlobalConstants.m_FrameNumber, wi, pdf);
 #endif
 
-    const RayPayload payload = TraceShadingRay(surface.m_Position, wi, MAX_DEPTH);
+    const RayPayload payload = TraceShadingRay(surface, wi, MAX_DEPTH);
     GIReservoirSample initialSample = GIReservoirSample::Empty();
     initialSample.m_Position = payload.m_HitPosition;
     initialSample.m_Normal = payload.m_HitNormal;
@@ -95,26 +52,6 @@ void RayGeneration()
     GIReservoir initialReservoir = GIReservoir::Empty();
     initialReservoir.Resample(initialSample, Random(screenCoords, g_GlobalConstants.m_FrameNumber), targetFunction, risWeight);
     g_RWOutputReservoir[sampleIdx] = GIReservoir::Pack(initialReservoir);
-}
-
-[shader("miss")]
-void Miss(inout RayPayload payload)
-{
-    payload.m_Hit = false;
-    payload.m_Radiance = 0;
-    payload.m_HitPosition = WorldRayOrigin() + WorldRayDirection() * 9999999.0f;
-
-    if (payload.m_IsShadowRay)
-    {
-        // Sample sun color
-        const float lerpFactor = saturate(dot(g_GlobalConstants.m_SunDirection.xyz, float3(0, 1, 0)));
-        payload.m_Radiance = lerp(0.0f, g_GlobalConstants.m_SunColor.xyz, lerpFactor);
-    }
-    else
-    {
-        // Sample environment color
-        payload.m_Radiance = SampleEnvironmentLighting(WorldRayDirection());
-    }
 }
 
 [shader("closesthit")]
@@ -129,7 +66,7 @@ void ClosestHit(inout RayPayload payload, in BuiltInTriangleIntersectionAttribut
     payload.m_Hit = true;
     payload.m_HitPosition = surface.m_Position;
     payload.m_HitNormal = surface.m_Normal;
-    payload.m_Depth = max(0, (int)payload.m_Depth - 1);
+    payload.m_Depth = max(0, (int) payload.m_Depth - 1);
     payload.m_Radiance = 0;
 
     if (payload.m_Depth <= 0)
@@ -138,7 +75,7 @@ void ClosestHit(inout RayPayload payload, in BuiltInTriangleIntersectionAttribut
     if (payload.m_IsShadowRay)
         return;
 
-    const RayPayload shadowRay = TraceShadowRay(surface);
+    const RayPayload shadowRay = TraceShadowRay(surface, g_GlobalConstants.m_SunDirection.xyz);
     const float3 wo = normalize(-WorldRayDirection());
     const float3 wi = normalize(g_GlobalConstants.m_SunDirection.xyz);
     payload.m_Radiance = surface.m_Emission + ComputeRadiance(surface, shadowRay.m_Radiance, wi, wo);
